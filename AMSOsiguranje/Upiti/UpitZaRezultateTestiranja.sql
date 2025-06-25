eb973d6d642911ea4647307162061269c6fd9cb5
Bogdan M <bogaman@hotmail.com>
2025-06-25 08:15:12 +0200

    Dodat folder sa upitima

---
+++ AMSOsiguranje/Upiti/UpitZaRezultateTestiranja.sql
@@ -0,0 +1,8 @@
+SELECT TOP (10) *
+  FROM [TestLogDB].[test].[tReportSumary]
+  ORDER BY [IdTestiranje] DESC;
+
+
+  SELECT TOP (10) *
+  FROM [TestLogDB].[test].[tReportIndividual]
+  ORDER BY [IdTest] DESC;
