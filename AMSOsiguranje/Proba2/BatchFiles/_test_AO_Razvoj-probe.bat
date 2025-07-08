3164691c5587d6fdd360ff8a4b1e2d033e2f93df
Bogdan M <bogaman@hotmail.com>
2025-07-08 15:02:49 +0200

    Izmene u kreiranju polise AO

--- AMSOsiguranje/Proba2/BatchFiles/_test_AO_Razvoj-probe.bat
+++ AMSOsiguranje/Proba2/BatchFiles/_test_AO_Razvoj-probe.bat
@@ -17,8 +17,8 @@ set NACIN_POKRETANJA=automatski
 ::dotnet test --filter FullyQualifiedName=Proba2.OsiguranjeVozila.AO_4_Polisa | findstr /i "Passed" >> Logovi\_log_AO_Proba2.txt 2>&1 
 
 echo. > Logovi\_log_AO_Proba2.txt
-echo [%DATE:~4,2%.%DATE:~7,2%.%DATE:~10,4%. %TIME%] Proba2.OsiguranjeVozila.AO_6_RazduznaLista... >> Logovi\_log_AO_Proba2.txt
-dotnet test --filter FullyQualifiedName=Proba2.OsiguranjeVozila.AO_6_RazduznaLista | findstr /i "Passed" >> Logovi\_log_AO_Proba2.txt 2>&1 
+echo [%DATE:~4,2%.%DATE:~7,2%.%DATE:~10,4%. %TIME%] Proba2.OsiguranjeVozila.AO_4_Polisa... >> Logovi\_log_AO_Proba2.txt
+dotnet test --filter FullyQualifiedName=Proba2.OsiguranjeVozila.AO_4_Polisa | findstr /i "Passed" >> Logovi\_log_AO_Proba2.txt 2>&1 
 
 ::echo. > Logovi\_log_AO_Proba2.txt
 ::echo [%DATE:~4,2%.%DATE:~7,2%.%DATE:~10,4%. %TIME%] AO_3_SE_UlazPrenosObrazaca... >> Logovi\_log_AO_Proba2.txt
--- AMSOsiguranje/UAT/BatchFiles/_test_AO_Razvoj-probe.bat
+++ AMSOsiguranje/UAT/BatchFiles/_test_AO_Razvoj-probe.bat
@@ -17,8 +17,8 @@ set NACIN_POKRETANJA=automatski
 ::dotnet test --filter FullyQualifiedName=UAT.OsiguranjeVozila.AO_4_Polisa | findstr /i "Passed" >> Logovi\_log_AO_UAT.txt 2>&1 
 
 echo. > Logovi\_log_AO_UAT.txt
-echo [%DATE:~4,2%.%DATE:~7,2%.%DATE:~10,4%. %TIME%] UAT.OsiguranjeVozila.AO_6_RazduznaLista... >> Logovi\_log_AO_UAT.txt
-dotnet test --filter FullyQualifiedName=UAT.OsiguranjeVozila.AO_6_RazduznaLista | findstr /i "Passed" >> Logovi\_log_AO_UAT.txt 2>&1 
+echo [%DATE:~4,2%.%DATE:~7,2%.%DATE:~10,4%. %TIME%] UAT.OsiguranjeVozila.AO_4_Polisa... >> Logovi\_log_AO_UAT.txt
+dotnet test --filter FullyQualifiedName=UAT.OsiguranjeVozila.AO_4_Polisa | findstr /i "Passed" >> Logovi\_log_AO_UAT.txt 2>&1 
 
 ::echo. > Logovi\_log_AO_UAT.txt
 ::echo [%DATE:~4,2%.%DATE:~7,2%.%DATE:~10,4%. %TIME%] AO_3_SE_UlazPrenosObrazaca... >> Logovi\_log_AO_UAT.txt
